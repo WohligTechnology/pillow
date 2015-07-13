02f57cb5ac988d205e732ecf66206e878641fe83
Tushar <tushar@wohlig.com>
2015-07-13 12:21:43 +0530

    ngDraggable implemented

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngDraggable'])
 
-.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
@@ -15,113 +15,134 @@ angular.module('starter.controllers', [])
     // Create the login modal that we will use later
     $ionicModal.fromTemplateUrl('templates/login.html', {
         scope: $scope
-    }).then(function (modal) {
+    }).then(function(modal) {
         $scope.modal = modal;
     });
 
     // Triggered in the login modal to close it
-    $scope.closeLogin = function () {
+    $scope.closeLogin = function() {
         $scope.modal.hide();
     };
 
     // Open the login modal
-    $scope.login = function () {
+    $scope.login = function() {
         $scope.modal.show();
     };
 
     // Perform the login action when the user submits the login form
-    $scope.doLogin = function () {
+    $scope.doLogin = function() {
         console.log('Doing login', $scope.loginData);
 
         // Simulate a login delay. Remove this and replace with your login
         // code if using a login system
-        $timeout(function () {
+        $timeout(function() {
             $scope.closeLogin();
         }, 1000);
     };
 })
 
-.controller('HomeCtrl', function ($scope, $ionicModal, $timeout) {
-        $ionicModal.fromTemplateUrl('templates/popup-design.html', {
-            scope: $scope,
-            animation: 'slide-in-up'
-        }).then(function (modal) {
-            $scope.modal = modal;
-        });
+.controller('HomeCtrl', function($scope, $ionicModal, $timeout) {
+    $ionicModal.fromTemplateUrl('templates/popup-design.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
 
-        $scope.openedit = function () {
-            $scope.modal.show();
-        };
+    $scope.openedit = function() {
+        $scope.modal.show();
+    };
 
-        $scope.closeModal = function () {
-            $scope.modal.hide();
-        };
-    })
-    .controller('CustomizeCtrl', function ($scope, $ionicModal, $timeout, $interval, $ionicPopup, $window) {
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
+})
 
-        //modal for picture
-        $ionicModal.fromTemplateUrl('templates/popup-design.html', {
-            scope: $scope,
-            animation: 'slide-in-up'
-        }).then(function (modal) {
-            $scope.modal = modal;
-        });
+.controller('CustomizeCtrl', function($scope, $ionicModal, $timeout, $interval, $ionicPopup, $window) {
 
-        $scope.Time = 150;
-        $scope.altTime = 125;
+    //ngDraggable
+    $scope.pillowImages = [{
+        name: 'one',
+        img: 'img/demo.jpg'
+    }, {
+        name: 'two',
+        img: 'img/demo1.jpg'
+    }, {
+        name: 'three',
+        img: 'img/demo.jpg'
+    }];
 
-        var promise;
-        $scope.mouseDown = function () {
-            promise = $interval(function () {
-                $scope.Time = $scope.Time + 1;
-                console.log($scope.Time);
-            }, 100);
+    $scope.onDropComplete = function(index, obj, evt) {
+        console.log(index);
+        var otherObj = $scope.pillowImages[index];
+        var otherIndex = $scope.pillowImages.indexOf(obj);
+        $scope.pillowImages[index] = obj;
+        $scope.pillowImages[otherIndex] = otherObj;
+    };
 
-        };
+    //modal for picture
+    $ionicModal.fromTemplateUrl('templates/popup-design.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
 
-        $scope.openedit = function () {
-            $scope.modal.show();
-        };
+    $scope.Time = 150;
+    $scope.altTime = 125;
 
-        $scope.closeModal = function () {
-            $scope.modal.hide();
-        };
+    var promise;
+    $scope.mouseDown = function() {
+        promise = $interval(function() {
+            $scope.Time = $scope.Time + 1;
+            console.log($scope.Time);
+        }, 100);
+
+    };
+
+    $scope.openedit = function() {
+        $scope.modal.show();
+    };
+
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
 
 
-        //Edit and Done button toggle
-        $scope.editimg = "true";
-        $scope.edit_img = function () {
-            $scope.edit = true;
-            $scope.doneimg = true;
-            $scope.editimg = false;
-            $scope.overlaydiv = true;
-        }
+    //Edit and Done button toggle
+    $scope.editimg = "true";
+    $scope.edit_img = function() {
+        $scope.edit = true;
+        $scope.doneimg = true;
+        $scope.editimg = false;
+        $scope.overlaydiv = true;
+    }
 
-        $scope.done_img = function () {
-            $scope.edit = false;
-            $scope.doneimg = false;
-            $scope.editimg = true;
-            $scope.overlaydiv = false;
-        }
-        $scope.mouseUp = function () {
-            $interval.cancel(promise);
-        };
-        //Popup for image selection
-        //        $scope.imgselected = function () {
-        //
-        //            var alertPopup = $ionicPopup.show({
-        //                title: "Image selected!",
-        //                //                template: 'Login Successfull'
-        //            });
-        //            $timeout(function () {
-        //                alertPopup.close(); //close the popup after 3 seconds for some reason
-        //            }, 3000);
-        //        }
+    $scope.done_img = function() {
+        $scope.edit = false;
+        $scope.doneimg = false;
+        $scope.editimg = true;
+        $scope.overlaydiv = false;
+    }
+    $scope.mouseUp = function() {
+        $interval.cancel(promise);
+    };
+    //Popup for image selection
+    //        $scope.imgselected = function () {
+    //
+    //            var alertPopup = $ionicPopup.show({
+    //                title: "Image selected!",
+    //                //                template: 'Login Successfull'
+    //            });
+    //            $timeout(function () {
+    //                alertPopup.close(); //close the popup after 3 seconds for some reason
+    //            }, 3000);
+    //        }
 
-        //Moving image in the mask image
-        $scope.moveImg = function (str, ishold) {
-            var step = 50; // change this to different step value
-            switch (str) {
+    //Moving image in the mask image
+    $scope.moveImg = function(str, ishold) {
+        var step = 50; // change this to different step value
+        switch (str) {
             case "down":
                 if (ishold == 0) {
                     var x = document.getElementById('img1').style.backgroundPositionY;
@@ -138,7 +159,7 @@ angular.module('starter.controllers', [])
                         document.getElementById('img1').style.backgroundPositionY = down + "px";
                     }
                 } else if (ishold == 1) {
-                    promise = $interval(function () {
+                    promise = $interval(function() {
                         var x = document.getElementById('img1').style.backgroundPositionY;
                         var index = x.indexOf("px");
                         console.log(index);
@@ -171,7 +192,7 @@ angular.module('starter.controllers', [])
                         document.getElementById('img1').style.backgroundPositionY = up + "px";
                     }
                 } else if (ishold == 1) {
-                    promise = $interval(function () {
+                    promise = $interval(function() {
                         var x = document.getElementById('img1').style.backgroundPositionY;
                         var index = x.indexOf("px");
                         console.log(index);
@@ -204,7 +225,7 @@ angular.module('starter.controllers', [])
                         document.getElementById('img1').style.backgroundPositionX = left + "px";
                     }
                 } else if (ishold == 1) {
-                    promise = $interval(function () {
+                    promise = $interval(function() {
                         var x = document.getElementById('img1').style.backgroundPositionX;
                         var index = x.indexOf("px");
                         console.log(index);
@@ -237,7 +258,7 @@ angular.module('starter.controllers', [])
                         document.getElementById('img1').style.backgroundPositionX = right + "px";
                     }
                 } else if (ishold == 1) {
-                    promise = $interval(function () {
+                    promise = $interval(function() {
                         var x = document.getElementById('img1').style.backgroundPositionX;
                         var index = x.indexOf("px");
                         console.log(index);
@@ -254,50 +275,56 @@ angular.module('starter.controllers', [])
                     }, 100);
                 }
                 break;
-            }
         }
+    }
 
-    })
-    .controller('CheckoutCtrl', function ($scope) {})
-    .controller('CartCtrl', function ($scope) {})
-    .controller('OrderCtrl', function ($scope) {})
-    .controller('RegisterCtrl', function ($scope) {})
-    .controller('LoginAccCtrl', function ($scope) {})
-    .controller('ProductCtrl', function ($scope, $ionicPopup, $timeout, $window) {
-        $scope.addcart = function () {
+})
 
-            var alertPopup = $ionicPopup.show({
-                title: "Added to cart!",
-                //                template: 'Login Successfull'
-            });
-            $timeout(function () {
-                alertPopup.close(); //close the popup after 3 seconds for some reason
-            }, 3000);
-        }
-    })
-    .controller('LoginCtrl', function ($scope) {})
-    .controller('PlaylistCtrl', function ($scope, $stateParams) {})
+.controller('CheckoutCtrl', function($scope) {})
 
-.directive('time', function ($interval) {
+.controller('CartCtrl', function($scope) {})
+
+.controller('OrderCtrl', function($scope) {})
+
+.controller('RegisterCtrl', function($scope) {})
+
+.controller('LoginAccCtrl', function($scope) {})
+
+.controller('ProductCtrl', function($scope, $ionicPopup, $timeout, $window) {
+    $scope.addcart = function() {
+
+        var alertPopup = $ionicPopup.show({
+            title: "Added to cart!",
+            //                template: 'Login Successfull'
+        });
+        $timeout(function() {
+            alertPopup.close(); //close the popup after 3 seconds for some reason
+        }, 3000);
+    }
+})
+
+.controller('LoginCtrl', function($scope) {})
+
+.directive('time', function($interval) {
     return {
         templateUrl: 'time.html',
         restrict: 'E',
         scope: {
             Time: '=value'
         },
-        link: function (scope, element, attrs) {
+        link: function(scope, element, attrs) {
             element.addClass('time');
 
             var promise;
-            scope.mouseDown = function () {
-                promise = $interval(function () {
+            scope.mouseDown = function() {
+                promise = $interval(function() {
                     scope.Time = scope.Time + 1;
                     console.log(scope.Time);
                 }, 100);
 
             };
 
-            scope.mouseUp = function () {
+            scope.mouseUp = function() {
                 $interval.cancel(promise);
             };
         }
