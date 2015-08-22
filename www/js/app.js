22dae61865f72f371c8d2bec0ba075ce8cdd3f7d
jagruti181 <patiljagruti181@gmail.com>
2015-08-22 13:39:24 +0530

    kuch to change

--- www/js/app.js
+++ www/js/app.js
@@ -11,7 +11,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'myservices'])
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
         }
         if (window.StatusBar) {
             StatusBar.overlaysWebView(true);
